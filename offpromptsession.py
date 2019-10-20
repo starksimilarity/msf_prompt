9ce92bc7e0bb3ad3541bc52bbd2bd4b008957637
starksimilarity <starksimilarity@gmail.com>
2019-10-20 10:05:01 -0500

    minor edits

--- offpromptsession.py
+++ offpromptsession.py
@@ -185,11 +185,11 @@ class OffPromptSession(PromptSession):
                         )
                         if override:
                             raise UserOverride(
-                                f"{self.current_user} overrode warning {e}"
+                                f"{self.current_user} overrode warning: {e}"
                             )
                         else:
                             raise UserOverrideDenied(
-                                f"{self.current_user} chose not to overide warning {e}"
+                                f"{self.current_user} chose not to overide warning: {e}"
                             )
                     else:
                         raise UserOverrideDenied(
@@ -216,11 +216,11 @@ class OffPromptSession(PromptSession):
                         )
                         if override:
                             raise UserOverride(
-                                f"{self.current_user} overrode warning {e}"
+                                f"{self.current_user} overrode warning: {e}"
                             )
                         else:
                             raise UserOverrideDenied(
-                                f"{self.current_user} chose not to overide warning {e}"
+                                f"{self.current_user} chose not to overide warning: {e}"
                             )
                     else:
                         raise UserOverrideDenied(
